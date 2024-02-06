0c5abe4be062e587bf0aecccdcc530b453edbdd4
CoraBlack <17833654450@163.com>
2024-02-06 09:55:50 +0800

    解决代码冲突 changed by Cora

--- Restaurant.cpp
+++ Restaurant.cpp
@@ -5,12 +5,6 @@ void Restaurant::SetCostomerMax(int num){cosmax = num; return;}
 
 void Restaurant::SetWareHouseMax(int num){warehouse = num; return;}
 
-<<<<<<< HEAD
 void Restaurant::SetOpenState(int state) { openstate = (bool)state; return; }
 
 bool Restaurant::GetOpenState() { return openstate; }
-=======
-void Restaurant::SetOpenState(bool state) { open = state; return; }
-
-bool Restaurant::GetOpenState() { return open; }
->>>>>>> c903271ae81cd1d839576afdfa3e32bd9ddd82a0
