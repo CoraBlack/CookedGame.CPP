fa99a770ab6cfb6fcafa0d01b5286243a4ef6bb7
CoraBlack <17833654450@163.com>
2024-02-07 17:50:38 +0800

    更新了冰箱 餐厅营业时的主要操作界面 后厨的操作界面 增加了冰箱内物品按名称顺序排序的功能（写了还没用上有小bug） changed by Cora

--- IngredientMarketUI.h
+++ IngredientMarketUI.h
@@ -3,37 +3,38 @@
 #include"MainGui.h"
 #include<vector>
 #include<string>
+//食材结构体
+struct ingredient {
+	std::string name;
+	unsigned grade;			//食材等级(1为低级,2为一般,3为高级)
+	float price;			//价格
+	int possession;			//拥有量
+};
 class IngredientMarketUI:public MainGui{
 public:
-//食材结构体
-	struct ingredient {
-		std::string name;
-		unsigned grade;			//食材等级(1为低级,2为一般,3为高级)
-		float price;			//价格
-		int possession;			//拥有量
-	};
+//成员函数
 	IngredientMarketUI();					//初始化构造函数
 	void IngredientMarket();				//食材市场
 	void ShoppingCartOperator();			//购物车的操作界面
-	void ChooseSomeIngredient(std::vector<ingredient>&vec);			//食材浏览列表
+	void ChooseSomeIngredient(std::vector<ingredient*>&vec);			//食材浏览列表
 	
 
 private:
 //食材
-	ingredient low_beef = { "低级牛肉",1,40,0 };
-	ingredient low_pork = { "低级猪肉",1,30,0 };
-	ingredient low_chicken = { "低级鸡肉",1,25,0 };
-	ingredient low_Chicken_chops = { "低级鸡扒",1,15,0 };
+	ingredient low_beef = { "牛肉(低级)",1,40,0 };
+	ingredient low_pork = { "猪肉(低级)",1,30,0 };
+	ingredient low_chicken = { "鸡肉(低级)",1,25,0 };
+	ingredient low_Chicken_chops = { "鸡扒(低级)",1,15,0 };
 
 	;
-	std::vector<ingredient>vegetable;						//蔬菜类食材
-	std::vector<ingredient>meat;							//肉类食材
-	std::vector <std::vector<ingredient>>all_ingredient;	//全部食材
+	std::vector<ingredient*>vegetable;						//蔬菜类食材
+	std::vector<ingredient*>meat;							//肉类食材
+	std::vector <std::vector<ingredient*>>all_ingredient;	//全部食材
 	;
 //存储数据
-	int page = 0;									//浏览列表页数(从零开始为第一页)
+	int page = 0;											//浏览列表页数(从零开始为第一页)
 	float total_price = 0;									//选购总价
-	std::vector<ingredient>shopping_cart;					//食材购物车
+	std::vector<ingredient*>shopping_cart;					//食材购物车
 };
 extern IngredientMarketUI* market_weight;
 #endif
--- Player.h
+++ Player.h
@@ -1,20 +1,23 @@
 //玩家Player类的设计
-#include<string>
 #ifndef PLAYER_H
 #define PLAYER_H
+#include<string>
+#include"IngredientMarketUI.h"
 class Player
 {
 public:
+	std::vector<ingredient*>hand;			//手持物品(食材)最多只能同时拿两个东西
 	void SetPlayerLocation(int loc);
-	void SetPlayerName(std::string str);//设置玩家名称
-	void SetPlayerMoney(float fl);//设置玩家钱财
-	std::string GetPlayerName();//获取玩家名称
-	float GetPlayerMoney();//获取玩家钱财
+	void SetPlayerName(std::string str);	//设置玩家名称
+	void SetPlayerMoney(float fl);			//设置玩家钱财
+	std::string GetPlayerName();			//获取玩家名称
+	float GetPlayerMoney();					//获取玩家钱财
 	
 private:
-	std::string name = "";
-	float money = 1000;
-	int location = 0;//玩家所在餐厅位置
+
+	std::string name = "";			//玩家名称
+	float money = 1000;				//玩家余额
+	int location = 0;				//玩家所在餐厅位置
 };
 extern Player* player;
 #endif // !PLAYER_H
